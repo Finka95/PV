d0d5abce24d8ba99ffbf810fd9d53f9c68ae8d8c
Vlad Pristavkin <138557453+VladPristavkin@users.noreply.github.com>
2024-06-24 10:57:22 +0300

    add medication operations

---
+++ HealthMate/HealthMate.BLL/Abstractions/IMedicationService.cs
@@ -0,0 +1,9 @@
+﻿using HealthMate.BLL.Models;
+
+namespace HealthMate.BLL.Abstractions
+{
+    public interface IMedicationService : IGenericService<Medication>
+    {
+        Task<ICollection<Medication>> GetByDate(DateOnly date, CancellationToken token);
+    }
+}
---
+++ HealthMate/HealthMate.BLL/Services/MedicationService.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HealthMate.BLL.Abstractions;
+using HealthMate.BLL.Models;
+using HealthMate.DAL.Abstractions;
+using HealthMate.DAL.Entities;
+
+namespace HealthMate.BLL.Services
+{
+    public class MedicationService(IMedicationRepository medicationRepository, IMapper mapper)
+        : GenericService<MedicationEntity, Medication>(medicationRepository, mapper), IMedicationService
+    {
+        public async Task<ICollection<Medication>> GetByDate(DateOnly date, CancellationToken token)
+        {
+            var medicationCollection = await medicationRepository.GetByDate(date, token);
+
+            return mapper.Map<ICollection<Medication>>(medicationCollection);
+        }
+    }
+}
