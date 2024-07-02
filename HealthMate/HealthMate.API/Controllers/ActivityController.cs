b0c2819e187ca0971663709d48c75618792ce9d9
Vlad Pristavkin <138557453+VladPristavkin@users.noreply.github.com>
2024-07-02 11:54:45 +0300

    add operations for actitvity

---
+++ HealthMate/HealthMate.API/Controllers/ActivityController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HealthMate.API.ViewModels.Activity;
+using HealthMate.BLL.Abstractions;
+using HealthMate.BLL.Models;
+
+namespace HealthMate.API.Controllers
+{
+    public class ActivityController
+        (IActivityService activityService, IMapper mapper)
+        : ModelWithNotesAndDateController<Activity, ActivityViewModel, ShortActivityViewModel>(activityService, mapper)
+    {
+    }
+}
---
+++ HealthMate/HealthMate.BLL/Services/ActivityService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HealthMate.BLL.Abstractions;
+using HealthMate.BLL.Models;
+using HealthMate.DAL.Abstractions;
+using HealthMate.DAL.Entities;
+
+namespace HealthMate.BLL.Services
+{
+    public class ActivityService
+        (IActivityRepository activityRepository, IMapper mapper)
+        : ModelWithNotesAndDateService<ActivityEntity, Activity>(activityRepository, mapper), IActivityService
+    {
+    }
+}
